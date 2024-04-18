b8ae23f7c065dfd8997a11fddd514e0276987b13
Eran Gazit <erang.sor@gmail.com>
2024-04-18 15:31:18 +0300

    add eldernet_ft to load the weights for the fine-tuned model

--- hubconf.py
+++ hubconf.py
@@ -26,24 +26,19 @@ def load_weights(weight_path, model, my_device="cpu"):
     model.load_state_dict(model_dict)
     print("%d Weights loaded" % len(pretrained_dict))
 
-def get_eldernet(pretrained=False, my_device="cpu", class_num=2, **kwargs):
+def eldernet_ssl(pretrained=False, my_device="cpu"):
     """
-    ElderNet model
-    pretrained (bool): kwargs, load pretrained weights into the model
+    load the SSL pretrained ElderNet (without the gait detection fine-tuning)
 
     Input:
-
-    X is of size: N x 3 x 300. N is the number of examples.
-    3 is the xyz channel. 300 consists of
-    a 10-second recording with 30hz.
+    pretrained (bool) : load ElderNet, pretrained on the MAP dataset
 
     Output:
-    my_device (str)
-    class_num (int): the number of classes to predict
+    model : the ElderNet model
 
     Example:
     repo = 'yonbrand/ElderNet'
-    model = torch.hub.load(repo, 'eldernet',pretrained=True)
+    model = torch.hub.load(repo, 'eldernet_ssl',pretrained=True)
     x = np.random.rand(1, 3, 300)
     x = torch.FloatTensor(x)
     model(x)
@@ -51,13 +46,33 @@ def get_eldernet(pretrained=False, my_device="cpu", class_num=2, **kwargs):
     """
     # Call the model, load pretrained weights
     feature_extractor = Resnet().feature_extractor
-    model = ElderNet(feature_extractor)
+    model = ElderNet(feature_extractor, is_eva=True)
     if pretrained:
         dirname = os.path.dirname(__file__)
-        checkpoint = os.path.join(
-            dirname, "", "model_check_point", "eldernet_weights.mdl"
-        )
+        checkpoint = os.path.join(dirname, "", "model_check_point", "ssl_weights.mdl")
         load_weights(checkpoint, model, my_device)
 
     return model
 
+
+def eldernet_ft(my_device="cpu"):
+    """
+    load  ElderNet fine-tuned on the Mobilise-D data for gait detection
+
+    Output:
+    model : the fine-tuned model
+
+    Example:
+    repo = 'yonbrand/ElderNet'
+    model = torch.hub.load(repo, 'eldernet_ft',pretrained=True)
+    x = np.random.rand(1, 3, 300)
+    x = torch.FloatTensor(x)
+    model(x)
+
+    """
+    feature_extractor = Resnet().feature_extractor
+    model = ElderNet(feature_extractor, is_eva=True)
+    dirname = os.path.dirname(__file__)
+    checkpoint = os.path.join(dirname, "", "model_check_point", "ft_weights.pt")
+    load_weights(checkpoint, model, my_device)
+    return(model)
